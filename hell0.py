0b60b33d46fc3a56dec0dc190228b663b8269785
shaojiayi8910 <49557804+shaojiayi8910@users.noreply.github.com>
2020-04-14 16:58:20 +0800

    Delete hell0.py

--- hell0.py
+++
@@ -1,9 +0,0 @@
-print("------shaojiayi-----")
-temp=input("input a number")
-guess=int(temp)
-if guess==8:
-    print("you are right")
-    print("let's contine")
-else:
-    print("you are wrong")
-print("game is over")
