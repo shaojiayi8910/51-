72e6a849bc1c5138014167353479fd1cf6f71427
shaojiayi8910 <49557804+shaojiayi8910@users.noreply.github.com>
2020-04-14 16:55:08 +0800

    Add files via upload

---
+++ hell0.py
@@ -0,0 +1,9 @@
+print("------shaojiayi-----")
+temp=input("input a number")
+guess=int(temp)
+if guess==8:
+    print("you are right")
+    print("let's contine")
+else:
+    print("you are wrong")
+print("game is over")
